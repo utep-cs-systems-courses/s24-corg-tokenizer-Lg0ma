f1e514372cf7a411d63ca7adc8ad806f006c4d4b
Luis Gomez <luisg31209@gmail.com>
2024-03-01 14:25:29 -0700

    redid addhistory to fix errors with previous iteration

--- src/history.c
+++ src/history.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "history.h"
+#define MAXTOKENS 100
 
 List* init_history()
 {
     List *history = malloc(sizeof(List));//allocates memory for List
-    history->root = malloc(sizeof(Item));//sets initial root pointer to null will be changed in futher functions
+    history->root = NULL;//sets initial root pointer to null will be changed in futher functions
     return history;
 }
 
 void add_history(List *list, char *str)
 {
-    int pos_id = 1;//creates position id at 1 to correctly assign it for later additions
-    Item *temp = list->root;//creates new Item named temp a copy of the root of the list
-    
-    while(temp)//traverses the array forward to get the position id of the new node and to move us to the tail of the list to add new Item
+    static int pos_id = 0;//declare variable pos_id to keep count of items to use count as item id
+    if(list != NULL)//if list exists
     {
-        temp = temp->next;
-        pos_id++;
+        Item *new_item = malloc(sizeof(Item));//declare new item and allocate size
+        if(new_item != NULL)//if new_item exists
+        {
+            new_item->id = pos_id;//set new_items id to pos_id
+
+            int i;
+            for(i = 0; i < MAXTOKENS && str[i] != '\0'; i++)//loop till the end of the str array is met or till MAXTOKENS is reached
+            {
+                new_item->str[i] = str[i];//assign str[i] in new_item to str[i](str array passed as parameter)
+            }
+            new_item->str[i] = '\0';//end the new_item's str array with zero terminator
+            new_item->next = NULL;//set next to NULL
+
+            if(list->root == NULL)//if list has no item in root
+            {
+                list->root = new_item;//If list has no items in it new_item will be the root
+            }
+            else
+            {
+                Item *temp = list->root;//create copy of list->root
+                while(temp->next != NULL)//iterate through temp unli the end of the list is met
+                {
+                    temp = temp->next;//advance to next item in list
+                }
+                temp->next = new_item;//add new_item to list
+            }
+            pos_id++;//increase static variable pos_id for next method call
+        }
     }
-    temp = malloc(sizeof(Item));//alloctes memory for new Item at the end of the list 
-    temp->id = pos_id;//assign pos_id to new Item 
-    temp->str = str;//stores token into the Item str variable
-    list = temp;
+    
 }
 
 char *get_history(List *list, int id)
